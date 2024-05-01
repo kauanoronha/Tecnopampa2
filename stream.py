adab3432722b0b6f91906c1700d93b12a30aa023
kauanoronha <kaua.reis.noronha@gmail.com>
2024-05-01 02:05:04 -0300

    ajustes de requerimentos

--- stream.py
+++ stream.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import pandas as pd
 
+#define configs da pagina
 st.set_page_config(
     layout="wide",
     initial_sidebar_state="auto",
 )
+#uploader do stream
 uploaded_file = st.file_uploader("Escolha seu arquivo",
                                  type=['xlsx'],
                                  )
+#valida o arquivo 
 if uploaded_file is not None:
     # To read file as bytes:
     try:
@@ -17,6 +20,6 @@ if uploaded_file is not None:
         df_agrupado = df_merged.to_excel('Dados2.xlsx', index=False)
         df = pd.read_excel('Dados2.xlsx')
         df1 = df.drop(index=22)
-        df1
+        
     except Exception as e:
         st.error(f"Erro ao ler o arquivo excel: {e}")
